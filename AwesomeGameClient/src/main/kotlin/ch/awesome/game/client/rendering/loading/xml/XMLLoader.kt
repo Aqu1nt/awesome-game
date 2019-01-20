9d4fec0079dda51e24ddad3286f03349e0657f89
Marino <Admin@nowhere>
2019-01-20 18:15:09 +0100

    forgot some resources. AGAIN!!

--- AwesomeGameClient/src/main/kotlin/ch/awesome/game/client/rendering/loading/xml/XMLLoader.kt
+++ AwesomeGameClient/src/main/kotlin/ch/awesome/game/client/rendering/loading/xml/XMLLoader.kt
@@ -1,4 +1,10 @@
 package ch.awesome.game.client.rendering.loading.xml
 
 class XMLLoader {
+
+    private val DATA = Regex(">(.+?)<")
+    private val START_TAG = Regex("<(.+?)>")
+    private val END_TAG = Regex("")
+    private val ATTRIBUTE_NAME = Regex("")
+    private val ATTRIBUTE_VALUE = Regex("")
 }
